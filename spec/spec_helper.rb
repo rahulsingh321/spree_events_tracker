e82da1108cc6c325f7924db8cba45688fa036900
Rahul <rahul97811@gmail.com>
2022-05-24 10:45:26 +0530

    More fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,23 +8,3 @@ require 'spree_dev_tools/rspec/spec_helper'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].sort.each { |f| require f }
-
-# RSpec.configure do |config|
-#   config.infer_spec_type_from_file_location!
-#   config.raise_errors_for_deprecations!
-#   config.use_transactional_fixtures = false
-
-#   config.include Spree::TestingSupport::UrlHelpers
-#   config.include Spree::TestingSupport::ControllerRequests, type: :controller
-
-#   config.include(Shoulda::Matchers::ActiveModel, type: :model)
-#   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-
-#   config.mock_with :rspec do |mock|
-#     mock.syntax = [:should, :expect]
-#   end
-#   config.color = true
-
-#   config.fail_fast = ENV['FAIL_FAST'] || false
-#   config.order = "random"
-# end
--- spree_events_tracker.gemspec
+++ spree_events_tracker.gemspec
@@ -25,5 +25,6 @@ Gem::Specification.new do |s|
   s.add_dependency 'spree_extension'
 
   s.add_development_dependency 'spree_dev_tools'
+  s.add_development_dependency 'pg'
   s.add_development_dependency 'net-smtp'
 end
